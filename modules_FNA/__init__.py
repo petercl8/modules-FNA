c7c930f25ede5fc5c3539ed26d9e8aee0d53fa88
petercl8 <lindstrom.peter@gmail.com>
2025-10-28 17:22:42 -0500

    Quick automatic update

--- modules_FNA/__init__.py
+++ modules_FNA/__init__.py
@@ -1,6 +1,8 @@
 # modules_FNA/__init__.py
 
 # Import all functions, classes, and trainables
+from . import functions
+
 from .functions import *
 
 # Optional: define __all__ for top-level import *
