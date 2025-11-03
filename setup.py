e0924ad2d4e557322ff99500c0b3ba6ca5187516
petercl8 <lindstrom.peter@gmail.com>
2025-11-03 14:57:58 -0600

    Quick automatic update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="modules_FNA",
     version="0.1",
-    packages=find_packages(include=["modules_FNA", "modules_FNA.*"]),
+    packages=find_packages(include=["Flexible_CNN_Architecture_for_Medical_Physics", "Flexible_CNN_Architecture_for_Medical_Physics.*"]),
     install_requires=[
         # Add external dependencies here, e.g. "numpy", "torch"
     ],
